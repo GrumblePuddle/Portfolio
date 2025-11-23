fbadee26eb43075a5282716f622189bd66cda91a
Gustav Liffner <gurra_liffner@hotmail.com>
2025-11-24 02:40:35 +0800

    Carusel added to NavLayout.jsx
    Added a temporary nav carusel with empty placeholders. Video or images will be added later on

--- portfolio/src/Components/NavLayout.jsx
+++ portfolio/src/Components/NavLayout.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import ProjectCarousel from "./ProjectCarusel";
 
 const NavLayout = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      {/* Bottom: fixed carousel */}
+      <div className="fixed bottom-0 left-0 right-0 z-20">
+        <ProjectCarousel />
+      </div>
     </div>
   );
 };
---
+++ portfolio/src/Components/ProjectCarusel.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const projects = [
+  {
+    id: 1,
+    title: "Projekt 1",
+    image: "/images/project1.jpg",
+    link: "/portfolio/project-1",
+  },
+  {
+    id: 2,
+    title: "Projekt 2",
+    image: "/images/project2.jpg",
+    link: "/portfolio/project-2",
+  },
+  {
+    id: 3,
+    title: "Projekt 3",
+    image: "/images/project3.jpg",
+    link: "/portfolio/project-3",
+  },
+  // lägg till fler projekt...
+];
+
+const ProjectCarousel = () => {
+  const items = [...projects, ...projects]; // duplicera för sömlös loop
+
+  return (
+    <div className="w-full overflow-hidden py-6">
+      <div className="group relative">
+        <div
+          className="
+            flex gap-6
+            animate-carousel-scroll
+            group-hover:[animation-play-state:paused]
+          "
+        >
+          {items.map((project, index) => (
+            <a
+              key={project.id + "-" + index}
+              className="shrink-0 w-64 rounded-2xl bg-white shadow-md overflow-hidden"
+            >
+              <div className="h-40 w-full overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCarousel;
